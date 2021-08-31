e7acf2caf3d2c2a32204d60326637a6e57eb611f
jrdefideveloper <ecom.jessicaju@gmail.com>
2021-08-30 20:09:13 -0700

    Has Single Cycle

--- medium-problems/SingleCycleCheck.java
+++ medium-problems/SingleCycleCheck.java
@@ -2,15 +2,29 @@ public class SingleCycleCheck {
 
     
     /** 
+     * O(n) time | O(1) space
      * @param array
      * @return boolean
      */
     public static boolean hasSingleCycle(int[] array) {
-        // Write your code here.
-        return false;
+        int numElementsVisited = 0; 
+        int currentIdx = 0; 
+
+        while (numElementsVisited < array.length) {  
+            if (numElementsVisited > 0 && currentIdx == 0) return false;  
+            numElementsVisited++; 
+            currentIdx = getNextIdx(currentIdx, array); 
+        }
+        return (currentIdx == 0);
+    }
+
+    public static int getNextIdx(int currentIdx, int[] array) { 
+        int jump = array[currentIdx];  
+        int nextIdx = (currentIdx + jump) % array.length;
+        return (nextIdx >= 0) ? nextIdx : nextIdx + array.length;  
     }
 
     public static void main(String[] args) {
-
+        System.out.println(hasSingleCycle(new int[] {2, 3, 1, -4, -4, 2})); 
     }
 }
